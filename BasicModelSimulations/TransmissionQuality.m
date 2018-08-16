a074fda9807f7c6e87f02f655a108dfd8a601969
Mohammadreza Mohagheghi <m.mohaghegh@gmail.com>
2018-08-16 11:22:02 +0200

    Adding simulations for binomial amplitude distribution.

--- BasicModelSimulations/TransmissionQuality.m
+++ BasicModelSimulations/TransmissionQuality.m
@@ -3,14 +3,23 @@
 % This script generates the results for the transmission quality for
 % exponential and binomial amplitude distributions
 
+addpath('./functions')
+
 % Exponential
 
 % Running the simulation; 1st input arg. specifies which chunk in
 % parameter space should be run and 2nd arg. specifies how many chunks
 % should the parameter space be devided into
 
-disp(['Running simulations to compute transmission quality', ...
-      ' for exponential amplitude distribution ...'])
+% disp(['Running simulations to compute transmission quality', ...
+%       ' for exponential amplitude distribution ...'])
+% 
+% res_dir = TCmodel_func_bwfor_modEXPgen(1, 1);
+% vis_res_lumped_mats(res_dir, 'EXP')
 
-res_dir = TCmodel_func_bwfor_modEXPgen(100, 560);
-vis_res_lumped_mats(res_dir, 'EXP')
+
+disp(['Running simulations to compute transmission quality', ...
+      ' for binomial amplitude distribution ...'])
+
+res_dir = TCmodel_func_bwfor(100, 420);
+vis_res_lumped_mats(res_dir, 'MIP')
